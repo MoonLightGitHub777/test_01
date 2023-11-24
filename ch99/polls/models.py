4a0a21b52f0d00370279a59cc82400e80fcadc76
SkyBlue <20230922>
2023-11-24 09:53:55 +0900

    Visual Studio Code로 Git 관리를 시작

--- ch99/polls/models.py
+++ ch99/polls/models.py
@@ -1,5 +1,13 @@
 from django.db import models
 
+"""
+<form action="/url/" method="post">
+    <label for ="your_name"?Your name: </label>
+    <input id="your_name" type="text" name="your_name">
+    <input type="submit" value="OK">
+</form>
+"""
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date_published')
@@ -12,4 +20,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-    
